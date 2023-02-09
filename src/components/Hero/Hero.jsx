9508b96990f4432de783964cebe93174072b4d0c
JMwira <jaymwira@gmail.com>
2023-02-09 10:11:03 +0200

    Hero_section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Hero.css'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import heart from "../../assets/heart.png"
+import calories from "../../assets/calories.png"
 const Hero = () => {
   return (
     <div className='hero' >
@@ -41,8 +45,29 @@ const Hero = () => {
                 <span>Fitness programs</span>
             </div>
         </div>
+        <div className="hero-buttons">
+            <button className="btn">Get started</button>
+            <button className="btn">Learn more</button>
+        </div>
       </div>
-      <div className="right-h">right</div>
+      <div className="right-h">
+        <button className="btn">Join now</button>
+        <div className="heart-rate">
+            <img src={heart} alt="" />
+            <span>Heart rate</span><span>BPM</span>
+        </div>
+        {/* hero images */}
+        <img src={hero_image} alt="" className='hero-image' srcset="" />
+        <img src={hero_image_back} alt="" className='hero-image-back' srcset="" />
+        {/* calories */}
+        <div className="calories">
+            <img src={calories} alt="" />
+            <div>
+                <span>Calories burned</span>
+                <span>220Kcal</span>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
