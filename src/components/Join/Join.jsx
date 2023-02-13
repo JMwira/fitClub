62489aacd05340f828715b4d4e630201f147b231
JMwira <jaymwira@gmail.com>
2023-02-13 09:30:10 +0200

    join-now-setup

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Join.css"
+
+const Join = () => {
+  return (
+    <div className="Join" id="Join-us">
+        <div className="left-j">
+            <hr/>
+            <div>
+                <span className='stroke-text' >READY TO </span>
+                <span>LEVEL UP</span>
+            </div>
+            <div>
+                <span>YOUR BODY </span>
+                <span className='stroke-text' >WITH US?</span>
+            </div>
+        </div>
+        <div className="right-j">
+            <form action="" className="email-container">
+                <input type="email" name="user-email" placeholder='enter your email'  />
+                <button className='btn btn-j' >Join now</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Join
